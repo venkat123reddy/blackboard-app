e6c3a7db53517e0eff58b06e128d1a10a4276b7c
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2025-05-26 18:12:58 -0500

    modifications

---
+++ FrontEnd/src/sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import './sidebar.css'
+
+
+
+
+function Sidebar() {
+
+
+    return (
+        <div className="sidebar">
+            Navigation
+        </div>
+    )
+
+}
+
+
+
+export default Sidebar;
