7e31cf8641a0aa2e55f4b23c24702fc95b73bfa5
Venkata Reddy Attala <36073543+venkat123reddy@users.noreply.github.com>
2025-05-26 16:20:56 -0500

    changes
    * template
    
    * models

---
+++ Backend/src/main/java/com/ucm/blackboard/models/User.java
@@ -0,0 +1,9 @@
+package com.ucm.blackboard.models;
+
+public abstract class User {
+
+    private String name;
+    private String email;
+    private Contact contact;
+    private UserType userType;
+}
